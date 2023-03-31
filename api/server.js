4ac601fb3a7c8d78d9ec49603b2714388ac07307
Brian Bissette <bbissette3@yahoo.com>
2023-03-31 10:59:46 -0500

    i dont even know anymore

--- api/server.js
+++ api/server.js
@@ -1,18 +1,24 @@
 // See https://github.com/typicode/json-server#module
-const path = require("path")
-const fs = require("fs");
-const os = require("os");
+// const path = require("path")
+// const fs = require("fs");
+// const os = require("os");
+
+
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3001
 
+setInterval(() => {
+    https.get("https://swolldiers-api.onrender.com/");
+  }, 300000); // every 5 minutes
 
-fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
-  if (err) console.log(err);
-  else console.log("copy file succeed to" + os.tmpdir());
-});
+// fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+//   if (err) console.log(err);
+//   else console.log("copy file succeed to" + os.tmpdir());
+// });
 
 server.use(middlewares)
 // Add this before server.use(router)
@@ -21,7 +27,7 @@ server.use(middlewares)
 //     '/blog/:resource/:id/show': '/:resource/:id'
 // }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log('JSON Server is running')
 })
 
